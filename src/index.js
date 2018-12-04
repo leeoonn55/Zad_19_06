5149e41d34c49aeac121d34eac1a19eae8c0d5b6
Leszek Macura <leszek@macura.co>
2018-12-04 21:22:28 +0100

    Zadanie 19_07 Drugie podejscie

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import DevTools from  './DevTools'
+
+
+const store = createStore(reducer, DevTools.instrument());
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -11,6 +16,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+
 
 
 ReactDOM.render(
